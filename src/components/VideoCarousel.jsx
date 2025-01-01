0f15d0c984331f88271d71ee93c22ad36ae569b5
sukumarss <sukumarjuly20@gmail.com>
2025-01-01 20:17:51 +0530

    VideoCourasal edited

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,5 +1,10 @@
 import {useRef,useState,useEffect} from 'react'
 import { hightlightsSlides } from '../constants'
+import { pauseImg, playImg, replayImg } from "../utils";
+import { useGSAP } from '@gsap/react';
+import {ScrollTrigger} from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+import gsap from "gsap";
 
 const VideoCarousel = () => {
     const videoRef = useRef([]);
@@ -18,6 +23,23 @@ const VideoCarousel = () => {
 
     const {isEnd,isLastVideo,startPlay,videoId,isPlaying}=video;
 
+
+    useGSAP(() =>{
+        gsap.to('#video', {
+            scrollTrigger: {
+                trigger: '#video',
+                toggleActions: 'restart none none none'
+            },
+                onComplete: () => {
+                    setVideo((pre) => ({
+                        ...pre,startPlay: true,
+                        isPlaying: true
+                    }));
+                },
+            })
+    },[isEnd,videoId]);
+
+
     useEffect(() =>{
         if(loadedData.length>3){
             if(!isPlaying){
@@ -27,6 +49,8 @@ const VideoCarousel = () => {
             }
         }
     },[startPlay,videoId,isPlaying,loadedData])
+
+    const handleLoadedMetadata = (i,e) => setLoadedData((pre) => [...pre,e])
 
     useEffect(() => {
         const currentProgress =0;
@@ -45,6 +69,33 @@ const VideoCarousel = () => {
         }
     },[videoId,startPlay])
 
+    const handleProcess = (type,i) => {
+        switch (type) {
+            case 'video-end' : 
+                setVideo((prevVideo) => ({...prevVideo,isEnd: 
+                    true,videoId: i+1
+                }))
+                break;
+            case 'video-last' : 
+                setVideo((pre) => ({...pre,isLastVideo:
+                    true
+                }))
+                break;
+            case 'video-reset' :
+                setVideo((pre) => ({...pre,isLastVideo: false,
+                    videoId: 0
+                }))
+                break;
+            case 'play' :
+                setVideo((pre) => ({...pre,isPlaying: !pre.isPlaying
+                    
+                }))
+                break;
+            default:
+                return video;
+        }
+    }
+
   return (
     <>
       <div className="flex items-center">
@@ -58,7 +109,8 @@ const VideoCarousel = () => {
                             setVideo((prevVideo) =>({
                                 ...prevVideo,isPlaying:true
                             }))
-                         }} >
+                         }}
+                         onLoadedMetadata={(e) => handleLoadedMetadata(i,e)} >
                             <source src={list.video} type="video/mp4" />
                         </video>
                     </div>
@@ -72,12 +124,34 @@ const VideoCarousel = () => {
         ))}
       </div>
       <div className="relative flex-center mt-10">
-        <div className = "flex-center mt">
-
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+          {videoRef.current.map((_, i) => (
+            <span
+              key={i}
+              className="mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer"
+              ref={(el) => (videoDivRef.current[i] = el)}
+            >
+              <span
+                className="absolute h-full w-full rounded-full"
+                ref={(el) => (videoSpanRef.current[i] = el)}
+              />
+            </span>
+          ))}
         </div>
+        <button className="control-btn">
+            <img src={isLastVideo ? replayImg :
+            !isPlaying ? playImg : pauseImg} alt={isLastVideo ? 'replay' :
+            !isPlaying ? 'play' : 'pause'}
+            onClick={isLastVideo
+            ? () => handleProcess('video-reset')
+            : !isPlaying
+              ? () => handleProcess('play')
+              : () => handleProcess('pause') }
+             />
+        </button>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default VideoCarousel
